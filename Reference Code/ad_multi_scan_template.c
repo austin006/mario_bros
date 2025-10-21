19529afc390d9e4c04dc1bd418c2e4182adf1f8e
austin006 <austinmcglashan1@gmail.com>
2025-10-21 09:07:20 -0600

    Added reference code for A/D conversion

---
+++ Reference Code/ad_multi_scan_template.c
@@ -0,0 +1,77 @@
+// This program reads from multiple A/D channels in
+// scan mode.
+
+#include <xc.h>
+
+#pragma config FNOSC = ??? // Select oscillator
+
+void config_ad(void);
+
+int main()
+{    
+    // Configure pins as analog inputs
+    // CODE HERE
+       
+    // Configure peripherals
+    config_ad(); 
+        
+    while(1)
+    {
+        // Read from the A/D buffers and do something useful
+        // with it. The A/D buffers are being updated continuously 
+        // behind the scenes.
+        // WRITE CODE TO DO SOMETHING INTERESTING WITH THE
+        // VALUES IN THE A/D BUFFERS HERE
+    }
+    
+    return 0;
+}
+
+void config_ad(void)
+{
+    
+    _ADON = 0;          // AD1CON1<15> -- Turn off A/D during config
+    
+    // Clear all A/D registers
+    AD1CON1 = 0; 
+    AD1CON2 = 0; 
+    AD1CON3 = 0; 
+    AD1CON5 = 0; 
+    AD1CSSL = 0; 
+    AD1CSSH = 0; 
+    
+    // AD1CON1 register
+    _ADSIDL = 0;    // AD1CON1<13> -- A/D continues while in idle mode
+    _MODE12 = 1;    // AD1CON1<10> -- 12-bit A/D operation
+    _FORM = 0;      // AD1CON1<9:8> -- Unsigned integer output
+    _SSRC = 7;      // AD1CON1<7:4> -- Auto conversion (internal counter)
+    _ASAM = 1;      // AD1CON1<2> -- Auto sampling
+
+    // AD1CON2 register
+    _PVCFG = 0;     // AD1CON2<15:14> -- Use VDD as positive ref voltage
+    _NVCFG = 0;     // AD1CON2<13> -- Use VSS as negative ref voltage
+    _BUFREGEN = 1;  // AD1CON2<11> -- Result appears in buffer
+                    // location corresponding to channel, e.g., AN12
+                    // results appear in ADC1BUF12
+    _CSCNA = 1;     // AD1CON2<10> -- Scans inputs specified in AD1CSSx
+                    // registers
+    _SMPI = ???;    // AD1CON2<6:2> -- Results sent to buffer after n conversion
+                    // For example, if you are sampling 4 channels, you
+                    // should have _SMPI = 3;
+    _ALTS = 0;      // AD1CON2<0> -- Sample MUXA only
+
+    // AD1CON3 register -- Change _SAMC and _ADCS based on your
+    // selection of oscillator and postscaling
+    _ADRC = 0;      // AD1CON3<15> -- Use system clock
+    _SAMC = 1;      // AD1CON3<12:8> -- Auto sample every A/D period TAD
+    _ADCS = 0;      // AD1CON3<7:0> -- A/D period TAD = TCY
+    
+    // AD1CHS register
+    _CH0NA = 0;     // AD1CHS<7:5> -- Measure voltages relative to VSS
+
+    // AD1CSSL register
+    // SET THE BITS CORRESPONDING TO CHANNELS THAT YOU WANT
+    // TO SAMPLE
+    
+    _ADON = 1;      // AD1CON1<15> -- Turn on A/D
+}
